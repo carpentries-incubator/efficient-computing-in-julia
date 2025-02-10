79c8ea8b601b97bf597a2a63d523e19cfbc72843
Johan Hidding <j.hidding@esciencecenter.nl>
2025-02-10 21:30:26 +0100

    update exercise script to output almost codimd compatible markdown

--- scripts/get-challenges.lua
+++ scripts/get-challenges.lua
@@ -1,9 +1,19 @@
+function Div(el)
+    if el.classes[1] == "solution" then
+        el.classes = {"spoiler"}
+    end
+    return el
+end
+
 function Pandoc(doc)
     local cblocks = {}
     table.insert(cblocks, pandoc.Header(1, doc.meta.title))
     for i, el in pairs(doc.blocks) do
-        if (el.t == "Div" and el.classes[1] == "challenge") or
-           (el.t == "Header") then
+        if (el.t == "Div" and el.classes[1] == "challenge") then
+            el.classes = {"info"}
+            table.insert(cblocks, el)
+        end
+        if (el.t == "Header") then
             table.insert(cblocks, el)
         end
     end
